aed41a717919515fcdd76a471e585c5a87f18384
Ankit Soni <akt_soni@yahoo.co.in>
2023-04-05 12:18:44 +0200

    footer added

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,13 +1,44 @@
+"use client";
 /***** IMPORTS *****/
 import React, { FC } from "react";
+import { createStyles, Group, ActionIcon, Flex } from "@mantine/core";
+import { BrandTwitter, BrandYoutube, BrandInstagram } from "tabler-icons-react";
 
-/***** TYPES *****/
-interface FooterProps {}
+import Section from "../common/Section";
+
+//styles
+const useStyles = createStyles((theme) => ({
+	footer: {
+		marginTop: "1rem",
+		borderTop: ` 1px solid ${theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]}`,
+		padding: "1rem",
+	},
+}));
 
 /***** COMPONENT-FUNCTION *****/
-const Footer: FC<FooterProps> = (): JSX.Element => {
+const Footer: FC = (): JSX.Element => {
+	const { classes } = useStyles();
 	/*** Return statement ***/
-	return <div>Footer</div>;
+	return (
+		<footer className={classes.footer}>
+			<Section>
+				<Flex align="center" justify="space-between">
+					<h4>LOGO</h4>
+					<Group spacing={0} position="right" noWrap>
+						<ActionIcon size="lg">
+							<BrandTwitter size="1.05rem" />
+						</ActionIcon>
+						<ActionIcon size="lg">
+							<BrandYoutube size="1.05rem" />
+						</ActionIcon>
+						<ActionIcon size="lg">
+							<BrandInstagram size="1.05rem" />
+						</ActionIcon>
+					</Group>
+				</Flex>
+			</Section>
+		</footer>
+	);
 };
 
 export default Footer;
