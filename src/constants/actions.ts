ae591afc37bfc73cbef6dffcf11be8abc2877713
Ankit Soni <akt_soni@yahoo.co.in>
2023-04-16 10:31:22 +0200

    Update actions.ts

--- src/constants/actions.ts
+++ src/constants/actions.ts
@@ -1,6 +1,6 @@
 export const getLocalTimeString = (dateString: string) => {
 	if (!dateString) return;
-	const date = new Date(dateString).toLocaleString("da-DK");
+	const date = new Date(dateString).toLocaleString("no-NO");
 
 	return date || "";
 };
